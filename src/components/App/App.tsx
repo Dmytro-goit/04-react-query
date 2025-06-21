c012c030e470f9c99dc048f00fe263f45c03a419
Dmytro-goit <dmytro.moiyseev@gmail.com>
2025-06-21 23:05:30 +0300

    Update App.tsx

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import ErrorMessage from "../ErrorMessage/ErrorMessage";
 import Loader from "../Loader/Loader";
 import MovieGrid from "../MovieGrid/MovieGrid";
 import MovieModal from "../MovieModal/MovieModal";
-import SearchBar from "../SearchBar/Searchbar";
+import SearchBar from "../SearchBar/SearchBar";
 import css from "./App.module.css";
 import type { Movie } from "../../types/movie";
 import toast, { Toaster } from "react-hot-toast";
